13b05b0914ee62a626a7fa94b0c3fa963f4c4faf
Oteayy <doroteasopic3@gmail.com>
2025-03-31 18:54:22 +0200

    Summary

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+def main():
+    # Pokreni aplikaciju
+    print('main() is working')
+
+if __name__ == '__main__':
+    # Inicijaliziraj bazu ako baza ne postoji
+    # Napuni bazu s osnovnim podacima
+    main()
---
+++ models/product_categories.py
@@ -0,0 +1,5 @@
+# TODO - Kreirati clasu koja cuva podatke o kategorijama proizvodima
+# te koja nasljeduje Base iz SQL Alchemy paketa
+
+class ProductCategory:
+    pass
